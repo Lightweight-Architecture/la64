743ce93d093f972aa370c1eacc658adf56f66a9f
Frida <157828245+cr4zyengineer@users.noreply.github.com>
2026-01-01 09:00:58 +0100

    fixing push,pop

--- src/instruction/data.c
+++ src/instruction/data.c
@@ -59,14 +59,14 @@ void la64_op_push(la64_core_t *core)
     la64_instr_termcond(core->op.param_cnt != 1);
     
     *((uint64_t*)&(core->machine->memory->memory[*(core->sp)])) = *(core->op.param[0]);
-    core->sp -= 8;
+    *(core->sp) -= 8;
 }
 
 void la64_op_pop(la64_core_t *core)
 {
     la64_instr_termcond(core->op.param_cnt != 1);
 
-    core->sp += 8;
+    *(core->sp) += 8;
     *(core->op.param[0]) = *((uint64_t*)&(core->machine->memory->memory[*(core->sp)]));
 }
 
